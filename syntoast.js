6a2bd5469d240b9e73c528876ff48cb62063247c
unknown <705H120@gmail.com>
2019-07-10 01:53:11 +0200

    init

--- syntoast.js
+++ syntoast.js
@@ -1,6 +1,7 @@
 var toast = {
 
   toast_container: null,
+  _timeout: null,
 
   init: function() {
     e_container = document.querySelector('.syntoast-container')
@@ -14,6 +15,21 @@ var toast = {
       this.toast_container = e_container
   },
 
+  remove: function(elem) {
+    elem.parentNode.removeChild(elem)
+  },
+
+  timeout: function(elem) {
+    var that = this
+    setTimeout(function(){
+      elem.classList.remove('slideInRight')
+      elem.classList.add('slideOutRight')
+      setTimeout(() => {
+        that.remove(elem)
+      }, 1000)
+    }, this._timeout);
+  },
+
   show: function(options) {
     if (options.type) {
 
@@ -21,12 +37,19 @@ var toast = {
     else
       options.type = 'infos'
 
+
+    if (options.timeout)
+      this._timeout = options.timeout
+    else
+      this._timeout = 10000;
     this.init()
 
 
     new_toast = document.createElement('div')
+    new_toast.classList.add('toast')
 
     new_toast_text = document.createElement('div')
+    new_toast_text.classList.add('toast-txt')
 
     new_toast_icon = document.createElement('div')
     new_toast_icon.classList.add('toast-symbol')
@@ -35,8 +58,11 @@ var toast = {
     new_toast_text.innerHTML = options.text
     new_toast.append(new_toast_text)
     new_toast.prepend(new_toast_icon)
+
+    new_toast.classList.add('slideInRight')
     this.toast_container.prepend(new_toast)
 
+    this.timeout(new_toast)
     console.log('e')
   }
 }
