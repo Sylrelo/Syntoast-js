dc210f86069e09d79c791365b22f346bd8585db6
unknown <705H120@gmail.com>
2019-07-12 15:36:24 +0200

    onclick callback, animation modification, title option

--- syntoast.js
+++ syntoast.js
@@ -3,6 +3,7 @@ var toast = {
   toast_container: null,
   _timeout: null,
   _dismissed: false,
+  options: null,
 
   init: function() {
     e_container = document.querySelector('.syntoast-container')
@@ -17,8 +18,8 @@ var toast = {
   },
 
   remove: function(elem) {
-    elem.classList.remove('slideInRight')
-    elem.classList.add('slideOutRight')
+    elem.classList.remove(this.options.animation_in)
+    elem.classList.add(this.options.animation_out)
     setTimeout(() => {
       elem.style.height = '0px'
       elem.style.padding = '0px'
@@ -53,38 +54,39 @@ var toast = {
     if (!options.animation_in)
       options.animation_in = 'slideInRight'
     if (!options.animation_out)
-      options.animation_out = 'slideOutRight'
+      options.animation_out = 'bounceOut'
 
+    this.options = options;
 
     this.init()
 
     new_toast = document.createElement('div')
     new_toast.classList.add('toast')
 
+    if (options.title) {
+      new_toast_title = document.createElement('div')
+      new_toast_title.classList.add('toast-title')
+      new_toast_title.innerHTML = options.title
+      new_toast.append(new_toast_title)
+    }
+
     new_toast_text = document.createElement('div')
     new_toast_text.classList.add('toast-txt')
-
     new_toast_icon = document.createElement('div')
     new_toast_icon.classList.add('toast-symbol')
     new_toast_icon.classList.add('toast-' + options.type)
-
     new_toast_text.innerHTML = options.text
-
-
     new_toast_progress = document.createElement('div')
     new_toast_progress.classList.add('toast-progress')
     new_toast_progress.style.animationDuration = this._timeout + 'ms'
-    new_toast.append(new_toast_progress)
     new_toast.append(new_toast_text)
     new_toast.prepend(new_toast_icon)
-
-    new_toast.classList.add('slideInRight')
-
-    new_toast.innerHTML = new_toast.innerHTML + '';
-
+    new_toast.append(new_toast_progress)
+    new_toast.classList.add(options.animation_in)
     new_toast.onclick = function() {
-      //that._dismissed = true
       that.remove(this)
+      if (options.callback)
+        callback()
     }
     this.toast_container.prepend(new_toast)
     this.timeout(new_toast)
